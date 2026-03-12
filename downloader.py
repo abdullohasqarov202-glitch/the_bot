1c715ec13d36b08bbb622f4bc2b7c25f55d674d8
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-12 10:38:07 +0300

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,23 @@
+import yt_dlp
+import os
+
+async def download_video(update, url):
+
+    await update.message.reply_text("⏳ Video yuklanmoqda...")
+
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': 'video.%(ext)s'
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+        for file in os.listdir():
+            if file.startswith("video"):
+                await update.message.reply_video(video=open(file,'rb'))
+                os.remove(file)
+
+    except:
+        await update.message.reply_text("❌ Yuklab bo'lmadi")
